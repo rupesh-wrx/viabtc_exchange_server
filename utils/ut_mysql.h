74bd33b3698fcedaf025e4cb55dbcda785c86feb
Rupesh Parab <rupesh.p@wazirx.com>
2021-11-26 16:54:51 +0530

    include

--- utils/ut_mysql.h
+++ utils/ut_mysql.h
@@ -7,7 +7,8 @@
 # define _UT_MYSQL_H_
 
 # include "ut_config.h"
-# include <mysql.h>
+# include <mysql/mysql.h>
+# include <mysql/errmsg.h>
 
 MYSQL *mysql_connect(mysql_cfg *cfg);
 bool is_table_exists(MYSQL *conn, const char *table);
